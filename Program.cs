9c512893dcc76f188da72c616b44b9fe031fe891
Oscar Negrete <oscar.negrete09@gmail.com>
2018-11-19 15:40:10 -0500

    Console UI

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace CSharpAsync
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+namespace CSharpAsync {
+  class Program {
+    static void Main (string[] args) {
+
+      Console.WriteLine ();
+      Console.WriteLine ("Simple Async Demo App");
+      Console.WriteLine ();
+      Console.WriteLine ("Press 1 for Normal Execute");
+      Console.WriteLine ("Press 2 for Async Execute");
+
+      var key = Console.ReadKey (true).Key;
+
+      if (key.ToString () == "D1") {
+        Console.WriteLine ("You selected Normal Execute");
+      } else if (key.ToString () == "D2") {
+        Console.WriteLine ("You selected Async Execute");
+      }
+
     }
+
+  }
 }
